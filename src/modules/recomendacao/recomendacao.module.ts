b43b4e8e5f1e88b9f7054bc48591d6b84cf3a463
Beatriz Egen <biahegen1@gmail.com>
2026-09-22 12:55:26 -0300

    fix: testes passando

--- src/modules/recomendacao/recomendacao.module.ts
+++ src/modules/recomendacao/recomendacao.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { RecomendacaoService } from './recomendacao.service.js';
 import { RecomendacaoController } from './recomendacao.controller.js';
+import { PrismaService } from '../../infra/prisma/prisma.service.js';
 
 @Module({
   controllers: [RecomendacaoController],
-  providers: [RecomendacaoService],
+  providers: [RecomendacaoService, PrismaService],
 })
 export class RecomendacaoModule {}
