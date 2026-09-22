bd52776493a962c40b349d07c3e818a0b7bf0e21
Isaac Hideki <hidekisaac1@gmail.com>
2026-09-22 20:02:24 -0300

    fix: restaura o arquivo main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule, ObserveInstrument } from './app.module.js';
 
 async function bootstrap() {
-
   const app = await NestFactory.create(AppModule, {
     instrument: ObserveInstrument,
   });
   app.enableCors();
-
-  //const observeEnabled = Boolean(
- // process.env.OBSERVE_APP_KEY && process.env.OBSERVE_APP_SECRET,
-//);
-
-
-
   await app.listen(process.env.PORT ?? 3333);
 }
-
 await bootstrap();
